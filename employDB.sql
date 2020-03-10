c0895d602f286943cad1238957a4589edfbea9c6
Owen <owenedwards422@gmail.com>
2020-03-10 17:12:26 -0400

    fixed sql files

--- employDB.sql
+++ employDB.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS employee_DB
-CREATE DATABASE employee_DB
-USE employee_DB
+DROP DATABASE IF EXISTS employee_DB;
+CREATE DATABASE employee_DB;
+USE employee_DB;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
@@ -13,7 +13,7 @@ CREATE TABLE roles (
     PRIMARY KEY (id),
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(9,2) NOT NULL,
-    department_id INT NOT NULL,
+    department_id INT NOT NULL
 );
 
 CREATE TABLE employees (
